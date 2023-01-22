395f6e62d4092069f339b14438a2684cd9820346
Christian Grimberg <christian.grimberg@outlook.com>
2023-01-22 13:03:46 +0000

    :+1: Start a second project to work with variables.

---
+++ src/02_02_variables/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _02_02_variables;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Let heightInMetres variable become equal to the value 1.88
+        double heightInMetres = 1.88;
+
+        Console.WriteLine($"The variable {nameof(heightInMetres)} has the value {heightInMetres}");
+    }
+}
